85bd4cd8660c3be37ff63c273ad8533cbed22b0d
TranHuuHieu15 <hieutranhuu.1505@gmail.com>
2024-05-19 22:03:18 +0700

    hieu lam cors cach 2 (tao bean)

--- src/main/java/hieutran/crud/config/AppConfig.java
+++ src/main/java/hieutran/crud/config/AppConfig.java
@@ -4,14 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class AppConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedOrigins("http://localhost:5500/", "http://127.0.0.1:5500/")
-                .allowedMethods("*")
-                .allowedMethods("*");
-    }
-}
+// Đây là cách một để câu hính cors trong Spring Boot
+//@Configuration
+//public class AppConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowCredentials(true)
+//                .allowedOrigins("http://localhost:5500/", "http://127.0.0.1:5500/")
+//                .allowedMethods("*")
+//                .allowedMethods("*");
+//    }
+//}
