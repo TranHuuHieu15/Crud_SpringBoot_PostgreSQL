660e00bb8074aebc8fdb930d735cea098c1cc3e7
TranHuuHieu15 <hieutranhuu.1505@gmail.com>
2024-05-19 22:32:42 +0700

    hieu lam cors cach 4 dung OncePerRequestFilter

--- src/main/java/hieutran/crud/config/AppConfig2.java
+++ src/main/java/hieutran/crud/config/AppConfig2.java
@@ -12,33 +12,33 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-@Configuration
-public class AppConfig2 {
-    //! Cách 2 để cấu hình CORS trong Spring Boot bằng bean
+//@Configuration
+//public class AppConfig2 {
+//    //! Cách 2 để cấu hình CORS trong Spring Boot bằng bean
+////    @Bean
+////    public WebMvcConfigurer corsFilter() {
+////        return new WebMvcConfigurer() {
+////            @Override
+////            public void addCorsMappings(CorsRegistry registry) {
+////                registry.addMapping("/**")
+////                        .allowCredentials(true)
+////                        .allowedOrigins("http://localhost:5500/", "http://127.0.0.1:5500/");
+////            }
+////        };
+////    }
+//    //! Cách 3 câu hình cors trong Spring Boot bằng crosFilter
 //    @Bean
-//    public WebMvcConfigurer corsFilter() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowCredentials(true)
-//                        .allowedOrigins("http://localhost:5500/", "http://127.0.0.1:5500/");
-//            }
-//        };
+//    public FilterRegistrationBean<CorsFilter> corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+////        config.addAllowedOrigin("http://127.0.0.1:5500/"); //! cho phép 1 domain này truy cập
+//        config.setAllowedOrigins(List.of("http://127.0.0.1:5500/", "http://localhost:5500/")); //! cho phép nhiều domain truy cập(Tương tự nhưng cái khác cũng vậy như setAllowedMethod)
+//        config.addAllowedMethod("*");
+//        config.addAllowedHeader("*");
+//        source.registerCorsConfiguration("/**", config); //cho phép tất cả domain truy cập
+//        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+//        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//        return bean;
 //    }
-    //! Cách 3 câu hình cors trong Spring Boot bằng crosFilter
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-//        config.addAllowedOrigin("http://127.0.0.1:5500/"); //! cho phép 1 domain này truy cập
-        config.setAllowedOrigins(List.of("http://127.0.0.1:5500/", "http://localhost:5500/")); //! cho phép nhiều domain truy cập(Tương tự nhưng cái khác cũng vậy như setAllowedMethod)
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
-        source.registerCorsConfiguration("/**", config); //cho phép tất cả domain truy cập
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
-    }
-}
+//}
