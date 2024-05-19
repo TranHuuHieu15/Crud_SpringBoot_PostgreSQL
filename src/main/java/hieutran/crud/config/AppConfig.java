b702173e84b3b5b2709451273751dbba496f66b4
TranHuuHieu15 <hieutranhuu.1505@gmail.com>
2024-05-19 22:23:59 +0700

    hieu lam cors cach 3 su dung UrlBasedCorsConfigurationSource va CorsConfiguration

--- src/main/java/hieutran/crud/config/AppConfig.java
+++ src/main/java/hieutran/crud/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// Đây là cách một để câu hính cors trong Spring Boot
+//! Đây là cách một để câu hính cors trong Spring Boot bằng implement WebMvcConfigurer
 //@Configuration
 //public class AppConfig implements WebMvcConfigurer {
 //    @Override
