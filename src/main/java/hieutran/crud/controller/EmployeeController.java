772ee120de1cff7e77897a140e0b20080608e537
TranHuuHieu15 <hieutranhuu.1505@gmail.com>
2024-05-20 22:23:19 +0700

    hieu lam OpenApi swagger

--- src/main/java/hieutran/crud/controller/EmployeeController.java
+++ src/main/java/hieutran/crud/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import hieutran.crud.config.Translator;
 import hieutran.crud.dto.EmployeeDto;
 import hieutran.crud.dto.response.ResponseSuccess;
 import hieutran.crud.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,17 +16,20 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/employees")
+@Tag(name = "Employee Controller", description = "API for Employee")
 public class EmployeeController {
 
     private EmployeeService employeeService;
 
     //! Tạo mới một employee
+    @Operation(summary = "Create a new employee", description = "Create a new employee")
     @PostMapping
     public ResponseSuccess<EmployeeDto> createEmployee(@Valid @RequestBody EmployeeDto employeeDto) {
         EmployeeDto saveEmployee = employeeService.createEntity(employeeDto);
         return new ResponseSuccess<>(HttpStatus.CREATED.value(), Translator.toLocale("employee.add.success"), saveEmployee);
     }
 
+    @Operation(summary = "Get one employee by id", description = "Get one employee by id")
     //! Lấy thông tin của một employee theo id
     @GetMapping("{employeeId}")
     public ResponseSuccess<EmployeeDto> getEmployeeById(@PathVariable("employeeId") Long employeeId) {
@@ -32,6 +37,7 @@ public class EmployeeController {
         return new ResponseSuccess<>(HttpStatus.OK.value(), Translator.toLocale("employee.getOne.success"), employeeDto);
     }
 
+    @Operation(summary = "Get all employees", description = "Get all employees")
     //! Lấy thông tin của tất cả employee
     @GetMapping
     public ResponseSuccess<List<EmployeeDto>> getAllEmployees() {
@@ -39,6 +45,7 @@ public class EmployeeController {
         return new ResponseSuccess<>(HttpStatus.OK.value(), Translator.toLocale("employee.getAll.success"), employees);
     }
 
+    @Operation(summary = "Update an employee by id", description = "Update an employee by id")
     //! Cập nhật thông tin của một employee
     @PutMapping("{employeeId}")
     public ResponseSuccess<String> updateEmployee(@PathVariable("employeeId") Long employeeId, @Valid @RequestBody EmployeeDto employeeDto) {
@@ -46,6 +53,7 @@ public class EmployeeController {
         return new ResponseSuccess<>(HttpStatus.OK.value(), Translator.toLocale("employee.update.success"));
     }
 
+    @Operation(summary = "Delete an employee by id", description = "Delete an employee by id")
     //! Xóa một employee
     @DeleteMapping("{employeeId}")
     public ResponseSuccess<String> deleteEmployee(@PathVariable("employeeId") Long employeeId) {
